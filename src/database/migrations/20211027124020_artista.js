1d4f14bff8bac809e6d01b0e33b09c467264d24a
Bernardo <bernardomaia@cpejr.com.br>
2021-10-28 15:39:12 -0300

    Finalização migrations

---
+++ src/database/migrations/20211027124020_artista.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("artista", function(table) {
+      table.string("artista_id").primary().notNullable();
+      table.string("nome").notNullable();
+      table.string("cd_id").notNullable();
+      table.foreign("cd_id").references("cd_id").inTable("cd").onDelete("NO ACTION");
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("artista");
+};
---
+++ src/database/migrations/20211028182219_carrinho.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("carrinho", function(table) {
+        table.string("carrinho_id").primary().notNullable();
+        table.string("user_id").notNullable();
+        table.foreign("user_id").references("user_id").inTable("user").onDelete("cascade");
+        table.string("cd_id").notNullable();
+        table.foreign("cd_id").references("cd_id").inTable("cd").onDelete("NO ACTION");       
+        });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("carrinho");
+};
