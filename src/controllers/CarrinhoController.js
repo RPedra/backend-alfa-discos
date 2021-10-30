6c66d0949e002d856445f0c7c596bc748fc50fb1
Bernardo <bernardomaia@cpejr.com.br>
2021-10-30 15:05:17 -0300

    Completando Controller Carrinho

--- src/controllers/CarrinhoController.js
+++ src/controllers/CarrinhoController.js
@@ -29,17 +29,38 @@ module.exports = {
 
     async update(request, response){
         try{
+            const {carrinho_id} = request.params;
+            const newCarrinho = request.body;
+            const result = await CarrinhoModel.updateById(carrinho_id, newCarrinho);
+            if (result === 0) return response.satus(400).json({notification: "Id não encontrado"});
+            return response.status(200).json({notification: "Carrinho atualizado!"});
 
         }catch(error){
-            
+            console.warn("Erro ao editar cd:", error);
+
+            return response
+            .status(500)
+            .json({
+                notification: "Erro ao editar CD"
+            });          
         }
     },
 
     async delete (request, response){
         try{
+            const {carrinho_id} = request.params;
+            const result = await CarrinhoModel.deleteById(carrinho_id);
+            if (result === 0) return response.satus(400).json({notification: "Id não encontrado"});
+            return response.status(200).json({notification: "Carrinho Excluído!"});
 
         }catch(error){
-            
+            console.warn("Erro ao tentar deletar carrinho:", error);
+
+            return response
+            .status(500)
+            .json({
+                notification: "Erro ao tentar deletar Carrinho"
+            });
         }
     },
 };
